eb2461066077e12a4b4bb36091d5f23fd27e8733
fabioportodev <fabioportodev@gmail.com>
2019-05-22 17:24:17 -0300

    Primeiro commit

---
+++ src/pages/signIn/index.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import { StatusBar, AsyncStorage } from 'react-native';
+import { StackActions, NavigationActions } from 'react-navigation';
+
+import api from '../../services/api';
+
+import {
+  Container,
+  Logo,
+  Input,
+  ErrorMessage,
+  Button,
+  ButtonText,
+  SignUpLink,
+  SignUpLinkText,
+} from './styles';
+
+
+export default class SignIn extends Component {
+  static navigationOptions = {
+      header: null,
+  };
+
+  state = { id: '', password: '', error: ''};
+
+  handleIdChange = (id) => {
+      this.setState({id});
+  };
+
+  handlePasswordChange = (password) => {
+      this.setState({ password });
+  };
+
+ 
+
+  handleSignInPress = async () => {
+    if (this.state.id.length === 0 || this.state.password.length === 0) {
+      this.setState({ error: 'Preencha usuário e senha para continuar!' }, () => false);
+    } else {
+      try {
+        const response = await api.post('/login', {
+          email: this.state.id,
+          password: this.state.password,
+        });
+
+        const resetAction = StackActions.reset({
+          index: 0,
+          actions: [
+            NavigationActions.navigate({ routeName: 'Main', params: { token: response.data.token } }),
+          ],
+        });
+        this.props.navigation.dispatch(resetAction);
+      } catch (_err) {
+        this.setState({ error: 'Houve um problema com o login, verifique suas credenciais!' });
+      }
+    }
+  };
+
+  render() {
+    return (
+      <Container>
+        <StatusBar hidden />
+        <Logo source={require('../../images/csa.png')} resizeMode="contain" />
+        <Input
+          placeholder="ID"
+          value={this.state.id}
+          onChangeText={this.handleIdChange}
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <Input
+          placeholder="Senha"
+          value={this.state.password}
+          onChangeText={this.handlePasswordChange}
+          autoCapitalize="none"
+          autoCorrect={false}
+          secureTextEntry
+        />
+        {this.state.error.length !== 0 && <ErrorMessage>{this.state.error}</ErrorMessage>}
+        <Button onPress={this.handleSignInPress}>
+          <ButtonText>Entrar</ButtonText>
+        </Button>
+        
+      </Container>
+    );
+  }
+}
